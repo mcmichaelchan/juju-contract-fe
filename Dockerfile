fd3848e3cb7894b2c0f2221674b8d4ed140f1c1d
mcmichaelchan <623769018@qq.com>
2018-11-27 22:46:20 +0800

    ci(docker): add docker support files

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+#stage 1
+FROM node:9.6.1 as react-build
+WORKDIR /app
+COPY . ./
+RUN yarn
+RUN yarn build
+
+#stage 2
+FROM nginx:alpine
+COPY nginx.conf /etc/nginx/conf.d/default.conf
+COPY --from=react-build /app/build /usr/share/nginx/html
+EXPOSE 80
+CMD ["nginx", "-g", "daemon off;"]
+
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,13 @@
 import React, { Component } from "react";
+import Web3 from "web3";
+
+const web3 = new Web3(window.web3.currentProvider);
+const accounts = web3.eth.getAccounts();
+
+(async () => {
+  const result = await accounts;
+  console.log(result[0]);
+})();
 
 class App extends Component {
   render() {
